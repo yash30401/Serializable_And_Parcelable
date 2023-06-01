1b0f4082f3900c7f46e6f1cf6eb115125aa02211
Yashveer singh <yashveersingh30401@gmail.com>
2023-06-01 19:29:41 +0530

    fix- non-null bug fixed

--- app/src/main/java/com/devyash/serializableandparcelable/GetDataActivity.kt
+++ app/src/main/java/com/devyash/serializableandparcelable/GetDataActivity.kt
@@ -1,5 +1,6 @@
 package com.devyash.serializableandparcelable
 
+import android.content.Intent
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -13,12 +14,13 @@ class GetDataActivity : AppCompatActivity() {
 
     private var _binding: ActivityGetDataBinding? = null
     private val binding get() = _binding!!
-
+    private var bundle: Bundle? =null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         _binding = ActivityGetDataBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        bundle = intent.extras
 
         getSerializableData()
         getParcelableData()
@@ -26,19 +28,20 @@ class GetDataActivity : AppCompatActivity() {
 
 
     private fun getSerializableData() {
-        val intent = intent.extras
-        val person = intent?.getSerializable("PERSON_S") as PersonWithSerializable
 
-        binding.tvId.text = "Id: ${person.id.toString()}"
-        binding.tvName.text = "Name: ${person.name.toString()}"
-        binding.tvAddress.text = "Address: ${person.address.toString()}"
-        binding.tvAge.text = "Age: ${person.age.toString()}"
+        val person = bundle?.getSerializable("PERSON_S") as? PersonWithSerializable
+
+        person?.let {
+            binding.tvId.text = "Id: ${it.id.toString()}"
+            binding.tvName.text = "Name: ${it.name.toString()}"
+            binding.tvAddress.text = "Address: ${it.address.toString()}"
+            binding.tvAge.text = "Age: ${it.age.toString()}"
+        }
 
     }
 
     private fun getParcelableData(){
-        val intent = intent.extras
-        val person = intent?.getParcelable<PersonWithParcelable>("PERSON_P")
+        val person = bundle?.getParcelable<PersonWithParcelable>("PERSON_P")
 
 
         person?.let {
