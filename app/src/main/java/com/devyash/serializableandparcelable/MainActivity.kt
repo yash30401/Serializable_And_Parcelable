3f861eb23493b4e8e5b890a98deaac39fa61e52c
Yashveer singh <yashveersingh30401@gmail.com>
2023-06-01 18:27:08 +0530

    Initial commit

---
+++ app/src/main/java/com/devyash/serializableandparcelable/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.devyash.serializableandparcelable
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.devyash.serializableandparcelable.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private var _binding:ActivityMainBinding?=null
+    private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+
+}
